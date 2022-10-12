05fb639c3643323f496059172874dbb5aabda786
bhikamchand <lkmdmick@gmail.com>
2022-10-12 21:20:49 +0530

    good

--- swastik 2.cpp
+++ swastik 2.cpp
@@ -8,7 +8,7 @@ right(90); wait(0.5);
 forward(100); wait(0.5);
 right(180); wait(0.5);
 forward(100); wait(0.5);
-right(-90); wait(0.5);
+left(90); wait(0.5);
 forward(100); wait(0.5);
 right(90); wait(0.5);
 }
